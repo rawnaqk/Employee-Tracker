566cd89280ad49e43568d2f4564c795fa272e758
rawnaqk <rawnaq.kabairzad@gmail.com>
2024-08-01 12:07:09 -0400

    created file structure, installed npm packages, and finished coding schema.sql file

---
+++ schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+\c employee_db;
+
+CREATE TABLE departments (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE roles (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  role_id INT REFERENCES roles(id),
+  manager_id INT REFERENCES employees(id)
+);
